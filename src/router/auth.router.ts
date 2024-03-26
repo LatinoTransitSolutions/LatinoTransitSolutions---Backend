ad41cc873450e0f2cc3f53f39efc1cf77c4ddd48
Harold Córdoba <hcordobaa@ucenfotec.ac.cr>
2024-03-25 20:33:06 -0600

    fix login

--- src/router/auth.router.ts
+++ src/router/auth.router.ts
@@ -1,19 +1,24 @@
 import { Request, Response, Router } from "express"
 import { createToken } from "../utils/auth.ts"
 import BaseResponse from "../common/BaseResponse.ts"
+import UserModel from "../database/models/UserModel.ts"
+import MariaDBConnection from "../database/connection/MariaDBConnection.ts"
+import { UserType } from "../types/User"
 
 const router: Router = Router()
 
+const model = new UserModel(MariaDBConnection.getInstance())
+
 router.post("/login", (req: Request, res: Response) => {
   const { email, password } = req.body
-  const userInDB = { id: 12345678, email: "admin", password: "password", role: "", name: "", company: "" }
-  const userFound = email === userInDB.email && password === userInDB.password ? userInDB : null
 
-  if (userFound) {
-    res.send(BaseResponse.success({ token: createToken(userFound) }))
-  } else {
-    res.send(BaseResponse.error("User not found"))
-  }
+  model.getByColumn({ email }).then(([userFound]: UserType[]) => {
+    if (userFound && userFound.password === password) {
+      res.send(BaseResponse.success({ token: createToken(userFound) }))
+    } else {
+      res.send(BaseResponse.error("User not found"))
+    }
+  })
 })
 
 export default router
